901569457eb17f405d3c0efe61f632ad99995915
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-17 20:47:03 +0300

    Update interfaceCANhack_a.c

--- Core/Src/interfaceCANhack_a.c
+++ Core/Src/interfaceCANhack_a.c
@@ -1,13 +1,20 @@
-/*
- * interfaceCANhack_a.c
- *
- *  Created on: Feb 9, 2024
- *      Author: mv904
- */
-/*
- modeIF - модефикация интерфейса
- updateIF
- */
+//----------------------------------------------------------------------------------------------------
+// Program for device CANhack
+// Copyright (C) 2024 YuriYuri (Yuriy Golovin)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//---------------------------------------------------------------------------------------------------
 #include  "interfaceCANhack_a.h"
 
 bool cursorFlag = 0;
@@ -319,3 +326,4 @@ void dataCANnumSet(uint64_t numCANdata)
 {
 	dataCANnum = numCANdata;
 }
+
