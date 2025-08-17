c13845433203315c0a576b9f32bc68a4576099ca
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-17 20:43:58 +0300

    Update buttonClickFun.h

--- Core/Inc/buttonClickFun.h
+++ Core/Inc/buttonClickFun.h
@@ -1,10 +1,20 @@
-/*
- * buttonClickFun.h
- *
- *  Created on: Feb 9, 2024
- *      Author: mv904
- */
-
+//----------------------------------------------------------------------------------------------------
+// Program for device CANhack
+// Copyright (C) 2024 YuriYuri (Yuriy Golovin)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------------------------------------
 #ifndef INC_BUTTONCLICKFUN_H_
 #define INC_BUTTONCLICKFUN_H_
 
@@ -27,3 +37,4 @@ bool iniCANflagKeySet();
 /*-----------------------------*/
 bool iniCANflagSet(bool flagCAN);
 #endif /* INC_BUTTONCLICKFUN_H_ */
+
