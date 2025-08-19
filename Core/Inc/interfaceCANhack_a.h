e33b0ce92414dae61171ca3b00198c3cd3d720ed
YuriYuriy <yuri.golovin.75@gmail.com>
2025-08-19 12:11:50 +0300

    Update interfaceCANhack_a.h

--- Core/Inc/interfaceCANhack_a.h
+++ Core/Inc/interfaceCANhack_a.h
@@ -1,21 +1,6 @@
 //----------------------------------------------------------------------------------------------------
-// Program for device CANhack
-// Copyright (C) 2024 YuriYuri (Yuriy Golovin)
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-//---------------------------------------------------------------------------------------------------
-
+//by YuriYuriy
+//----------------------------------------------------------------------------------------------------
 #ifndef INC_INTERFACECANHACK_A_H_
 #define INC_INTERFACECANHACK_A_H_
 
@@ -49,4 +34,5 @@ void bomberTIMEnumSet(uint16_t bomberTIME);
 void dataCANnumSet(uint64_t numCANdata);
 
 #endif /* INC_INTERFACECANHACK_A_H_ */
+
 
